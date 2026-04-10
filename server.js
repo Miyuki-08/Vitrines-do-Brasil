0c35618efcda59fc4dd5b846868fdaa078fbe496
Miyuki-08 <siprianomaeda@gmail.com>
2026-04-10 01:53:58 -0300

    fix rota raiz

--- server.js
+++ server.js
@@ -25,6 +25,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/', (req, res) => {
+  res.send('FUNCIONANDO 🚀');
+});
+
 // criar agendamento
 app.post('/agendar', async (req, res) => {
   const { nome, telefone, data, hora } = req.body;
