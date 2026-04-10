c60f462fc4eabec0e543ac7fa0ff9e3da5c05103
Miyuki-08 <siprianomaeda@gmail.com>
2026-04-10 01:39:51 -0300

    fix: rota inicial

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors());
@@ -85,6 +86,6 @@ app.get('/horarios/:data', async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('Servidor rodando na porta 3000');
+app.listen(PORT, () => {
+  console.log('Servidor rodando');
 });
